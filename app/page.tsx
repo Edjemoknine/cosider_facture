c80a52d003472eedce82495f62bf116152d3baa4
moknine <mokninemoknine999@gmail.com>
2024-03-03 13:28:09 +0100

    add error handling and suspense boundries for seamless streaming

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import TableInv from "@/components/shared/Table";
 import { getData } from "@/lib/Fetch";
 import { InvoiceType } from "@/types";
+import { Suspense } from "react";
 
 export default async function Home() {
   const data: InvoiceType[] = await getData();
   return (
     <main className="max-w-6xl mx-auto p-6">
-      <TableInv data={data} />
+      <Suspense fallback="Loading...">
+        <TableInv data={data} />
+      </Suspense>
     </main>
   );
 }
--- lib/Fetch.ts
+++ lib/Fetch.ts
@@ -1,5 +1,9 @@
 export const getData = async () => {
-  const resp = await fetch(`https://elhoussam.github.io/invoicesapi/db.json`);
-  const data = await resp.json();
-  return data;
+  try {
+    const resp = await fetch(`https://elhoussam.github.io/invoicesapi/db.json`);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
