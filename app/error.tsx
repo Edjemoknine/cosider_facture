1730bf3d2416ba0753ba86ac8e5a788be233cc27
moknine <mokninemoknine999@gmail.com>
2024-03-03 14:00:06 +0100

    add Error type

--- app/error.tsx
+++ app/error.tsx
@@ -2,15 +2,15 @@
 import { Button } from "@/components/ui/button";
 import React, { useEffect } from "react";
 
-const Error = ({ error, reset }) => {
+const Error = ({ error, reset }: { error: any; reset: any }) => {
   useEffect(() => {
     console.log(error);
   }, [error]);
 
   return (
-    <div className="text-center flex flex-col gap-5 justify-center">
+    <div className="text-center mt-16 flex flex-col gap-5 justify-center items-center">
       <h1 className="text-3xl font-semibold"> Opps!Somethingwent wrong</h1>
-      <Button className="mt-6" onClick={() => reset()}>
+      <Button className="mt-6 w-fit" onClick={() => reset()}>
         Try Again
       </Button>
     </div>
